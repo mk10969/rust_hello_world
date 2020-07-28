c761b935e14728cd43f009943c889b2cc42e06f0
mk10969 <mk10969@gmail.com>
2020-07-29 00:05:11 +0900

    add

--- hello_rust/src/test/iter_test.rs
+++ hello_rust/src/test/iter_test.rs
@@ -1,5 +1,9 @@
+use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
+use std::io;
+use std::iter::{once, repeat};
+use std::ops::Range;
 use std::str::FromStr;
 
 #[test]
@@ -144,8 +148,194 @@ fn test_iterator2() {
     flat_map_test.insert("baz", vec![10, 11, 12, 13]);
     println!("=================");
     flat_map_test.iter()
+        .inspect(|r| println!("doOnNext: {:?}", r)) // doOnNext()
         .flat_map(|r| r.1)
         .for_each(|i| println!("{:?}", i));
 }
 
+
+#[test]
+fn test_iterator_scan() {
+    (0..=20)
+        .filter_map(|n|
+            if n % 2 == 1 {
+                Some(n * n)
+            } else {
+                None
+            })
+        .for_each(|n| println!("{}, ", n));
+
+    // 0 ~ 10 まで実行しない。途中で打ち切ることができる。
+    let iter = (0..=10)
+        .scan(0, |sum, item| {
+            *sum += item;
+            if *sum > 10 {
+                None
+            } else {
+                Some(item * item)
+            }
+        }).collect::<Vec<i32>>();
+
+    println!("{:?}", iter);
+    // assert_eq!(iter.collect::Vec<>(), vec![]);
+}
+
+
+#[test]
+fn test_iterator_chain() {
+    let v: Vec<i32> = (1..4).chain(vec![20, 30, 40]).collect();
+    assert_eq!(v, vec![1, 2, 3, 20, 30, 40]);
+}
+
+
+#[test]
+fn test_iterator_zip() {
+    let endings = vec!["once", "twice", "chicken soup with rice"];
+    let rhyme = repeat("going")
+        .zip(endings).collect::<Vec<_>>();
+    // println!("{:?}", rhyme);
+    assert_eq!(rhyme, vec![("going", "once"),
+                           ("going", "twice"),
+                           ("going", "chicken soup with rice")]);
+
+    // cloned
+    let a = ['a', 'b', 'c', 'd'];
+    assert_eq!(a.iter().inspect(|i| println!("{:?}", i)).next(), Some(&'a'));
+    assert_eq!(a.iter().cloned().inspect(|i| println!("{:?}", i)).next(), Some('a'));
+}
+
+#[test]
+fn test_iterator_cycle() {
+    let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
+    let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
+    let v = fizzes.zip(buzzes);
+
+    (1..=100).zip(v)
+        // .inspect(|i| println!("{:?}", i))
+        .map(|tuple| match tuple {
+            (i, ("", "")) => i.to_string(),
+            (_, (fizz, buzz)) => format!("{}{}", fizz, buzz)
+        })
+        .for_each(|line| print!("{}, ", line));
+
+    fn fizzbuzz(n: u32) -> String {
+        match n {
+            n if n % 15 == 0 => "fizzbuzz".to_string(),
+            n if n % 3 == 0 => "fizz".to_string(),
+            n if n % 5 == 0 => "buzz".to_string(),
+            n => n.to_string()
+        }
+    }
+    (1..=100).map(fizzbuzz).for_each(|n| print!("{}, ", n));
+}
+
+
+#[test]
+fn test_iter_max_min() {
+    // 2つを比較する。Nanなら、panic!
+    // アイテムから取り出すとき参照で、クロージャにはさらにその参照を渡すから、＆＆にする
+    fn cmp(lhs: &&f64, rhs: &&f64) -> Ordering {
+        lhs.partial_cmp(rhs).unwrap()
+    }
+
+    let number = [1.0, 4.0, 3.0];
+
+    assert_eq!(number.iter().max_by(cmp), Some(&4.0));
+    assert_eq!(number.iter().min_by(cmp), Some(&1.0));
+
+    let number2 = [1.0, 4.0, std::f64::NAN, 3.0];
+    // assert_eq!(number2.iter().min_by(cmp), ); //panic
+}
+
+#[test]
+fn test_iter_max_min_key() {
+    let mut populations = HashMap::new();
+    populations.insert("Portland", 583_776);
+    populations.insert("Fossil", 449);
+    populations.insert("Greenhorn", 2);
+    populations.insert("Boring", 7_762);
+    populations.insert("The Dalles", 15_340);
+
+    let num = populations.iter().max_by_key(|&(key, value)| value);
+    assert_eq!(num, Some((&"Portland", &583_776)));
+    let num2 = populations.iter().min_by_key(|&(key, value)| value);
+    assert_eq!(num2, Some((&"Greenhorn", &2)));
+}
+
+#[test]
+fn test_iter_any_all() {
+    let id = "Iterator";
+    assert!(id.chars().any(char::is_uppercase));
+    assert!(!id.chars().all(char::is_uppercase));
+}
+
+#[test]
+fn test_position_rposition_exactSizeIterator() {
+    let text = "Xerxes";
+    // 左から検索して、ヒットしたポジション（index）を返却する    indexは 0から始まる
+    assert_eq!(text.chars().position(|c| c == 'e'), Some(1));
+    assert_eq!(text.chars().position(|c| c == 'a'), None);
+
+    // 右から検索する。
+    let bytes = b"Xerxes";
+    assert_eq!(bytes.iter().rposition(|c| c == &b'e'), Some(4));
+
+    // &strは、可変長エンコーディングなため、rpositionが使えない（ExactSizeIteratorを実装していない）
+    // しかし、バイト配列に対するイテレータは、配列長がわかっているので、ExactSizeIteratorを実装している
+}
+
+#[test]
+fn test_iter_fold() {
+    let a = [5, 6, 7, 8, 9, 10];
+    assert_eq!(a.iter().fold(0, |a, _| a + 1), 6); // count
+    assert_eq!(a.iter().fold(0, |a, &b| a + b), 45); //sum
+    assert_eq!(a.iter().fold(1, |a, b| a * b), 151200); //product
+
+    // max
+    assert_eq!(a.iter().fold(i32::min_value(), |m, &n| std::cmp::max(m, n)), 10);
+
+    let b = ["Pack ", "my ", "box ", "with ", "five ", "dozen."];
+    let pangram = b.iter().fold(String::new(), |mut s, &i| {
+        s.push_str(i); //&strだとpushできない
+        s
+    });
+    println!("{}", pangram);
+}
+
+#[test]
+fn test_iter_nth_last() {
+    let mut spuares = (0..10).map(|i| i * i);
+    assert_eq!(spuares.nth(4), Some(16));
+    assert_eq!(spuares.nth(0), Some(25)); // 0は、next()と等価
+    assert_eq!(spuares.nth(6), None); //なくなると、None
+
+    let once = (0..10).map(|i| i * i);
+    assert_eq!(once.last(), Some(81));
+    // ↑これは、全部スキャンするので、iter().rev().next()がよい。
+}
+
+#[test]
+fn test_iter_find() {
+    let mut populations = HashMap::new();
+    populations.insert("Portland", 583_776);
+    populations.insert("Fossil", 449);
+    populations.insert("Greenhorn", 2);
+    populations.insert("Boring", 7_762);
+    populations.insert("The Dalles", 15_340);
+
+    // 最初に見つかったもの、1つだけ返す。いまいち・・・
+    println!("{:?}", populations.iter().find(|&(name, &pop)| pop < 1000));
+}
+
+#[test]
+fn test_iter_partition(){
+    let things = ["doorknob", "mushroom", "noodle", "giraffe", "grapefruit"];
+    // iterを返すのではなく、collectを返す。さらに、型指定する必要がある。上同じ型でなければならない。
+    let (living, nonliving): (Vec<&str>, Vec<&str>) = things
+        .iter()
+        .partition(|name| name.as_bytes()[0] & 1 != 0);
+
+    println!("{:?}", living);
+    println!("{:?}", nonliving);
+}
 
